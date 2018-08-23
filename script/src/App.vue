c7ab6376b42d7617c155d380dfa0058b0174cc90
myisliuyuchuan <2583828122@qq.com>
2018-08-23 22:31:10 +0800

    hhh

--- script/src/App.vue
+++ script/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+      111111111111111111
   </div>
 </template>
 
@@ -11,7 +10,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- script/src/main.js
+++ script/src/main.js
@@ -3,7 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+//element ui框架
+import element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(element)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

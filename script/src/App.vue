b5c390d25bd9b869bfc2244d76063885073163aa
刘予川 <705506725@qq.com>
2018-09-13 14:43:09 +0800

    aaa

--- script/src/App.vue
+++ script/src/App.vue
@@ -34,20 +34,16 @@
       beforeCreate(){
       //添加一个请求拦截器
         this.axios.interceptors.request.use((config)=>{
-          //在请求发送之前做一些事
           this.isLoading = true;
           return config;
         },function(error){
-          //当出现请求错误是做一些事
           return Promise.reject(error);
         });
         //添加一个返回拦截器
         this.axios.interceptors.response.use((response) => {
-          //对返回的数据进行一些处理
           this.isLoading = false;
           return response;
         },function(error){
-          //对返回的错误进行一些处理
           return Promise.reject(error);
         });
       },

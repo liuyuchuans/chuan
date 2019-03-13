9229ec74cddaec42018131fa226284a8c00cb88a
= <=>
2019-03-13 16:52:36 +0800

    更换图片

---
+++ vue-user/src/view/404/404.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-container>
+        <router-link to="/">
+            <img src="../../assets/403/404.gif">
+        </router-link>
+        
+    </el-container>
+</template>
+<script>
+export default {
+    name: '404',
+    created(){
+           
+    },
+    methods: {
+        
+    }
+}
+</script>
+<style scoped>
+    img{
+        width: 100%;
+        height: 100%;
+    }
+  
+</style>

3f555af25d243fa3f1296fa562a50ede942fbc9b
aichuanz <2583828122@qq.com>
2019-03-24 21:52:50 +0800

    map bug 解决

--- vue-user/src/store/model/tab/tab.js
+++ vue-user/src/store/model/tab/tab.js
@@ -12,7 +12,7 @@ export default{
                     return;
             });
 
-            if(data.fullPath !== "/404" && data.fullPath !== "/login")
+            if(data.fullPath !== "/" && data.fullPath !== "/404" && data.fullPath !== "/login")
                 state.list.push({
                     title: data.meta.title,
                     name: (new Date().getTime()),

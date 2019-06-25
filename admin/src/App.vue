dbab33d5f059d10b02a71cce745d1ab11ba2472f
aichuanz <2583828122@qq.com>
2019-06-25 23:32:39 +0800

    ADMIN

---
+++ admin/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  
+}
+</script>
+
+<style>
+
+</style>
